f7a09b093eb2a1a1890a8c2fe0d31ff459512b0f
kcubani1 <ubanichukwu@gmail.com>
2022-10-13 12:34:40 -0700

    correction again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/bah
 #include <stdio.h>
-
 /**
  *main - Entry point
  *
@@ -8,6 +7,8 @@
  */
 int main(void)
 {
+	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }	
