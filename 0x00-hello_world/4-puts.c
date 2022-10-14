3994202c521d5051abd93ce1686d416b59e7b36c
kcubani1 <ubanichukwu@gmail.com>
2022-10-14 10:22:00 -0700

    commiting git commit -mcommiting
    git commit -mcommiting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 /**
- *
- *  *main- Serves as the entry point for the program
- *
- *   *
- *
- *   *Return: By default zero if no erros otherwise returns non-zero value.
+ *  *main - Entry point
+ *  *Return: By default zero if no erros otherwise returns non-zero value.
  *
  *  **/
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
