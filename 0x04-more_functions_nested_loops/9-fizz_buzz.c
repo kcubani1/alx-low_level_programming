c9f3d163283f27867c7fb1f6dcf9e26655c00eb4
kcubani1 <ubanichukwu@gmail.com>
2022-10-21 05:36:03 -0700

    corrected

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 
 /**
- * main  - the entry point
- * Return: 0 if the program run with success
+ *  main - fizz bizz holterton style
+ *  Return: 0 if no errors
  */
 int main(void)
 {
-	int a = 0, b = 0, c = 0;
+	int i;
 
-	for (a = 1; a < 101; a++)
+	for (i = 1; i <= 100; i++)
 	{
-		b = a % 3;
-		c = a % 5;
-		if (b == 0 && c == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if (c == 0 && b != 0)
-			printf("Buzz");
-		else if (b == 0 && c != 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-			printf("%d", a);
-		if (a < 100)
-			putchar(' ');
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
-	putchar('\n')
+	printf("\n");
 	return (0);
 }
 
