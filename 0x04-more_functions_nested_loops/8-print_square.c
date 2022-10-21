5fd45ea0d001edf2debdffad31d730b06543a344
kcubani1 <ubanichukwu@gmail.com>
2022-10-21 05:02:19 -0700

    printer

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,24 @@
-#include "main"
+#include "main.h"
+
 /**
- *  print_square - function that checks for uppercase character.
- *   @size: the int for the paramaters of my function
- *  Return: 0
- */
+ *print_square - A function who print a square
+ *@size: size of square
+ *Return: Does not return anything
+*/
 void print_square(int size)
 {
-	int a = 0;
-	int b = 0;
+	int i, j;
+	int n = size;
 
-	if (size > 0)
+	if (n > 0)
 	{
-		while (a < size)
+		for (j = 0; j < n; j++)
 		{
-			while (b < size)
+			for (i = 0; i < n; i++)
 			{
-				_putchar('#');
-				b++;
+				_putchar(35);
 			}
-		b = 0;
-		a++;
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 	else
